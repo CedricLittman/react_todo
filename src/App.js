b1af76ca9b23294e0e0a9389d20b3c42408ae251
CedricLittman <cedric@littman.me.uk>
2025-04-29 18:42:34 +0100

    Tasks can be input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React from "react";
 import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+    inputTask.current.value = "";
+  };
+
+  const inputTask = useRef(null);
+  /*
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+      inputTask.current.value = "";
+    }
+  };
+
+  const addTask = () => {
+    if (currentTask.trim() === "") {
+      alert("Please enter a task.");
+      return;
+    }
+    setTodoList([...todoList, currentTask]);
+    setCurrentTask("");
+  };
+*/
+
   return (
     <div className="App">
       <h1>Todo List</h1>
       <p>This is a simple React application.</p>
       <div>
-        <input type="text" placeholder="Enter Task" />
-        <button>Add Task</button>
+        <input
+          ref={inputTask}
+          type="text"
+          placeholder="Enter Task"
+          onChange={(event) => {
+            setCurrentTask(event.target.value);
+          }}
+        />
+        <button onClick={addTask}>Add Task</button>
       </div>
       <hr />
+      <ul>
+        {todoList.map((val, key) => {
+          return <li key={key}> {val}</li>;
+        })}
+      </ul>
     </div>
   );
 }
